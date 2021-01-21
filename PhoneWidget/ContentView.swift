a8c61146e2f76fd60b74615cca352089926e6b43
Emilio Peláez <me@epelaez.net>
2021-01-21 21:52:29 +0100

    Request contacts access

--- PhoneWidget/ContentView.swift
+++ PhoneWidget/ContentView.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
-import CoreData
+import Contacts
 
 struct ContentView: View {
+	let store = CNContactStore()
+	
+	@State var authorizationFailed = false
 	
 	var body: some View {
 		Text("Hello")
+			.onAppear {
+				switch CNContactStore.authorizationStatus(for: .contacts) {
+				case .notDetermined:
+					store.requestAccess(for: .contacts) { success, _ in
+						if !success {
+							self.authorizationFailed = true
+						}
+					}
+				case .denied, .restricted:
+					DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
+						self.authorizationFailed = true
+					}
+				case _: break
+				}
+			}
+			.alert(isPresented: $authorizationFailed) {
+				Alert(title: Text("Contact Access Denied"),
+							message: Text("You'll need to grant contacts permissions to use this app. You can do this from settings."),
+							primaryButton: .cancel(Text("Ok")),
+							secondaryButton: .default(Text("Open Settings"), action: self.openSettings))
+			}
+	}
+	
+	func openSettings() {
+		guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+		UIApplication.shared.open(url)
 	}
 	
 }
@@ -19,6 +48,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+		ContentView(authorizationFailed: false)
 	}
 }
