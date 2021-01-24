1888179ea5439c1e20fc88aadcaf7a7acf7b06d9
Emilio Peláez <me@epelaez.net>
2021-01-24 20:33:40 +0100

    Automatically scroll instructions when showing more

--- PhoneWidget/TutorialView.swift
+++ PhoneWidget/TutorialView.swift
@@ -13,74 +13,83 @@ struct TutorialView: View {
 	
 	var body: some View {
 		NavigationView {
-			ScrollView {
-				VStack(alignment: .leading, spacing: 15) {
-					Group {
-						TutorialStepView(title: "Step 1",
-														 content: "On your Home Screen or the Lock Screen, tap and hold on an Icon, Widget or an empty space until you see this icon on the top left of your screen. Tap it!",
-														 caption: "Tip: Holding on an empty space is faster!")
-						Divider()
-					}
-					Group {
-						TutorialStepView(title: "Step 2",
-														 content: "Find Phone Widget in the list and tap on it.",
-														 caption: "Tip: You can also use the search bar!")
-						Divider()
-					}
-					.transition(.slide)
-					.hidden(currentStep < 2)
-					Group {
-						TutorialStepView(title: "Step 3",
-														 content: "Tap Add Widget",
-														 caption: "Tip: You can also drag the widget into your Home or Lock Screen!")
-						Divider()
-					}
-					.transition(.slide)
-					.hidden(currentStep < 3)
-					Group {
-						TutorialStepView(title: "Step 4",
-														 content: "If your widget is dancing, tap on it to configure it. If it's static, tap and hold, then select Edit Widget.",
-														 caption: nil)
-						Divider()
-					}
-					.transition(.slide)
-					.hidden(currentStep < 4)
-					Group {
-						TutorialStepView(title: "Step 5",
-														 content: "Configure your widget with an image size, a color, a contact and your call options.",
-														 caption: "Tip: If the name is too long, too short, or you want to use a nickname, use the optional Alternative Name field!")
-						Divider()
-					}
-					.transition(.slide)
-					.hidden(currentStep < 5)
-					Group {
-						TutorialStepView(title: "Done!",
-														 content: "Your widget is ready! Now tap on it whenever you want to make a call!",
-														 caption: nil)
-					}
-					.transition(.slide)
-					.hidden(currentStep < 6)
-					HStack {
-						Spacer()
-						Button {
-							withAnimation {
-								self.currentStep += 1
-							}
-						} label: {
-							if currentStep < totalSteps - 1 {
-								Text("Next Step")
-							} else {
-								Text("Final Step")
-							}
+			ScrollViewReader { scroll in
+				ScrollView {
+					VStack(alignment: .leading, spacing: 15) {
+						Group {
+							TutorialStepView(title: "Step 1",
+															 content: "On your Home Screen or the Lock Screen, tap and hold on an Icon, Widget or an empty space until you see this icon on the top left of your screen. Tap it!",
+															 caption: "Tip: Holding on an empty space is faster!")
+							Divider()
 						}
-						.padding()
+						Group {
+							TutorialStepView(title: "Step 2",
+															 content: "Find Phone Widget in the list and tap on it.",
+															 caption: "Tip: You can also use the search bar!")
+							Divider()
+						}
+						.transition(.slide)
+						.hidden(currentStep < 2)
+						Group {
+							TutorialStepView(title: "Step 3",
+															 content: "Tap Add Widget",
+															 caption: "Tip: You can also drag the widget into your Home or Lock Screen!")
+							Divider()
+						}
+						.transition(.slide)
+						.hidden(currentStep < 3)
+						Group {
+							TutorialStepView(title: "Step 4",
+															 content: "If your widget is dancing, tap on it to configure it. If it's static, tap and hold, then select Edit Widget.",
+															 caption: nil)
+								Divider()
+						}
+						.transition(.slide)
+						.hidden(currentStep < 4)
+						Group {
+							TutorialStepView(title: "Step 5",
+															 content: "Configure your widget with an image size, a color, a contact and your call options.",
+															 caption: "Tip: If the name is too long, too short, or you want to use a nickname, use the optional Alternative Name field!")
+							Divider()
+						}
+						.transition(.slide)
+						.hidden(currentStep < 5)
+						Group {
+							TutorialStepView(title: "Done!",
+															 content: "Your widget is ready! Now tap on it whenever you want to make a call!",
+															 caption: nil)
+						}
+						.transition(.slide)
+						.hidden(currentStep < 6)
+						HStack {
+							Spacer()
+							Button {
+								withAnimation(Animation.easeInOut(duration: 0.15)) {
+									self.currentStep += 1
+									DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+										withAnimation {
+											scroll.scrollTo("Bottom")
+										}
+									}
+								}
+							} label: {
+								if currentStep < totalSteps - 1 {
+									Text("Next Step")
+								} else {
+									Text("Final Step")
+								}
+							}
+							.padding()
+							Spacer()
+						}
+						.transition(.slide)
+						.hidden(currentStep >= 6, remove: true)
 						Spacer()
+							.id("Bottom")
 					}
-					.transition(.slide)
-					.hidden(currentStep >= 6, remove: true)
+					.padding()
+					.navigationTitle("How to Use")
 				}
-				.padding()
-				.navigationTitle("How to Use")
 			}
 		}
 	}
