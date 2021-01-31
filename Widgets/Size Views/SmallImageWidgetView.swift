3def0b7053a7e314bacb42cde3c4def95098a42c
Emilio Peláez <me@epelaez.net>
2021-01-31 18:30:31 +0100

    Improve small image layout

--- Widgets/Size Views/SmallImageWidgetView.swift
+++ Widgets/Size Views/SmallImageWidgetView.swift
@@ -14,37 +14,40 @@ struct SmallImageWidgetView : View {
 	var body: some View {
 		ZStack {
 			LinearGradient(gradient: entry.background.gradient, startPoint: .top, endPoint: .bottom)
-			VStack(spacing: 2) {
+			VStack(spacing: 4) {
 				HStack {
-					ZStack {
-						Color.white
-							.opacity(0.25)
-						Image(systemName: "person.fill")
-						entry.image?
-							.resizable()
-							.aspectRatio(contentMode: .fill)
+					Group {
+						if let image = entry.image {
+							image
+								.resizable()
+								.aspectRatio(contentMode: .fit)
+						} else {
+							
+						}
 					}
-					.frame(width: 36, height: 36)
-					.cornerRadius(18)
+					.clipShape(Circle())
 					Spacer()
 				}
 				HStack {
 					Text(entry.name)
 						.lineLimit(2)
+						.layoutPriority(1)
 						.font(.system(size: 25, weight: .semibold, design: .default))
 					Spacer()
 				}
+				.layoutPriority(1)
 				HStack(spacing: 4) {
 					entry.connection.image
 					Text(entry.connection.name)
 						.lineLimit(1)
 						.minimumScaleFactor(0.5)
-						.font(.system(size: 15, weight: .bold, design: .default))
+						
 					Spacer()
 				}
+				.font(.system(size: 14, weight: .bold, design: .default))
 				.opacity(0.5)
 			}
-			.padding(.horizontal)
+			.padding()
 			.foregroundColor(.white)
 		}
 	}
