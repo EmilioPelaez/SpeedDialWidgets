77574556b5d831e381a82993584c0594be449046
Emilio Peláez <me@epelaez.net>
2021-02-03 10:58:46 +0100

    Improve display when user didn’t select an action

--- Widgets/Utilities/Connection+UI.swift
+++ Widgets/Utilities/Connection+UI.swift
@@ -15,35 +15,32 @@ extension Connection {
 	
 	var image: Image {
 		switch self {
-		case .phone, .faceTimeAudio: return Image(systemName: "phone.fill")
+		case .phone, .faceTimeAudio, .unknown: return Image(systemName: "phone.fill")
 		case .faceTime: return Image(systemName: "video.fill")
 		case .message, .whatsapp: return Image(systemName: "message.fill")
 		case .telegram: return Image(systemName: "paperplane.fill")
-		case .unknown: return Image(systemName: "questionmark.circle.fill")
 		}
 	}
 	
 	var name: String {
 		switch self {
-		case .phone: return "Phone"
+		case .phone, .unknown: return "Phone"
 		case .faceTime: return "FaceTime"
 		case .faceTimeAudio: return "Audio"
 		case .message: return "Messages"
 		case .whatsapp: return "WhatsApp"
 		case .telegram: return "Telegram"
-		case .unknown: return "Unknown"
 		}
 	}
 	
 	var scheme: String {
 		switch self {
-		case .phone: return "tel://"
+		case .phone, .unknown: return "tel://"
 		case .faceTime: return "facetime://"
 		case .faceTimeAudio: return "facetime-audio://"
 		case .message: return "sms://"
 		case .whatsapp: return "https://wa.me/"
 		case .telegram: return "tg://msg?to="
-		case .unknown: return "tel://"
 		}
 	}
 	
