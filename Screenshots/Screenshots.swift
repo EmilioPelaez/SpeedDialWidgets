59f10575e44add51d9854a9a2ba7b26f7b41ed12
Emilio Peláez <me@epelaez.net>
2022-04-19 22:52:20 +0200

    Add Screenshots Target and Scheme

---
+++ Screenshots/Screenshots.swift
@@ -0,0 +1,18 @@
+//
+//  Screenshots.swift
+//  Screenshots
+//
+//  Created by Emilio Peláez on 19/04/22.
+//
+
+import XCTest
+
+class Screenshots: XCTestCase {
+	func testExample() throws {
+		// UI tests must launch the application that they test.
+		let app = XCUIApplication()
+		app.launch()
+		
+		// Use XCTAssert and related functions to verify your tests produce the correct results.
+	}
+}
